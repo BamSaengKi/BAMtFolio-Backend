2a2f4a1f7f9067e83754f4876e1a2600b9dd59c6
bamsaengki <bamsaengki@gmail.com>
2022-05-05 18:21:17 +0900

    [UPDATE] reotes get post put update

--- routes/portfolio/portfolio.controller.js
+++ routes/portfolio/portfolio.controller.js
@@ -1,22 +1,121 @@
 var express = require('express');
 var router = express.Router();
 
+//  포트폴리오 기본값
 var PortfolioLst =[
   {
-    id : 1,
+    id : "bamsaengki",
     title : "프로젝트명",
     duration : "2022-01-01 ~ 2022-02-01",
     lang : "java, js",
     part : "90%",
     thumbnail : "",
-    content : ""
+    content : "",
+    updateat : new Date()
   }
 
 ]
 
-/* GET users listing. */
+// 글 작성
+router.post('/', (req,res)=> {
+  try {
+    const body = req.body;
+    const {id} = body;
+
+    const temp = PortfolioLst.filter(item =>{
+      return id === item.id;
+    });
+
+    if(temp.length != 0){
+      throw new Error("ㅋㅋ 아이디 하나당 게시물은 하나밖에 적을 수 ㅇ벗으셈 ㅋㅋ")
+    }
+
+    const newPortpolio = {
+      ...body,
+      updateat : new Date()
+    }
+
+    PortfolioLst.push(newPortpolio);
+
+    res.status(200).json({
+      status : 200,
+      data : newPortpolio,
+      message : "SUCCESS"
+    })
+  } catch (error) {
+      res.status(400).json({
+        status : 400,
+        message : error.message
+      })
+  }
+})
+
+
+
+// 포트폴리오 수정
+
+router.put('/', (req, res)=>{
+  try {
+    const body = req.body;
+    const {id} = body;
+
+    const temp = PortfolioLst.filter(item =>{
+      return id === item.id;
+    })
+
+    if(temp.length ===0){
+      throw new Error("수정한 정보가 없습니다.")
+    }
+
+    const updatePortfolio ={
+      ...body,
+      updateat : new Date()
+    }
+
+    const updatelist = PortfolioLst.map(item =>{
+      if(id === item.id){
+        return updatePortfolio;
+      }
+
+      return item;
+    })
+
+      PortfolioLst = updatelist
+
+    res.status(200).json({
+      status : 200,
+      data : updatelist,
+      message : "SUCCESS"
+    })
+    
+  } catch (error) {
+    res.status(400).json({
+      status : 200,
+      message : error.message
+    })
+  }
+
+})
+
+// 포트폴리오 조회
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  try {
+    res.status(200).json({
+    status : 200,
+    data : PortfolioLst,
+    message : "SUCCESS"
+    });
+      
+  } catch (error) {
+    res.status(400).json({
+      status : 400,
+      message : error.message
+    })
+    
+  }
+  
 });
+
+
 
 module.exports = router;
--- routes/users/users.controller.js
+++ routes/users/users.controller.js
@@ -1,18 +1,119 @@
 var express = require('express');
 var router = express.Router();
 
-var user = {
+// 유저 기본값
+var user  = [
+{
   id: "bamsaengki",
   name : "오진우",
   birth : "1994-11-16",
   lang : "java, JS",
   title : "test",
-  decription : "test"
+  description : "test",
+  createdat : new Date("2022-01-01"),
+  updateat : new Date()
 }
 
-/* GET users listing. */
+]
+// 회원가입
+router.post('/', (req, res)=> {
+  try {
+    const body = req.body;
+    const {id} = body;
+
+    const temp = user.filter(item =>{
+      return id === item.id;
+    });
+
+      if(temp.length != 0){
+        throw new Error("이미 가입된 아이디입니다.");
+      }
+
+  const newUser = {
+    ...body,
+    createdat : new Date()
+  }
+
+  user.push(newUser);
+
+    res.status(200).json({
+      status : 200,
+      data : newUser,
+      message : "SUCCESS"
+    })
+  } catch (error) {
+    res.status(400).json({
+      status : 400,
+      message : error.message
+    })   
+    
+  }
+})
+
+
+// 회워정보 수정
+router.put('/', (req, res)=>{
+  try {
+    const body = req.body;
+    const {id} = body;    
+
+    const temp = user.filter(item =>{
+      return id === item.id;
+    });
+
+    if(temp.length ===0){
+      throw new Error("수정할 정보가 없습니다.")
+    }
+
+    const updateUser = {
+      ...body,
+      updateat : new Date()
+    }
+    
+    const updatelist = user.map(item =>{
+      if(id === item.id){
+        return updateUser;
+      }
+
+        return item;
+      
+    });
+
+
+      user = updatelist
+  
+
+    res.status(200).json({
+      status : 200,
+      data : updateUser,
+      message : "SUCCESS"
+    })
+  } catch (error) {
+    res.status(400).json({
+      status : 400,
+      message : error.message
+    })
+    
+  }
+})
+
+
+/* 회원 조회  200 : ok   300 : 중복   400 : 클라이언트 오류   500 : 서버 오류*/
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+  try {
+    res.status(200).json({
+      status : 200,
+      data : user,
+      message : "SUCCESS"
+    })    
+  } catch (error) {
+    res.status(400).json({
+      status : 400,
+      message : error.message
+    })    
+  }
+  });
+  
+
 
 module.exports = router;
